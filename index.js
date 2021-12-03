b63b4cbf240319d4ebc7902659005546e8887281
zcaldwell <mintyst.john@gmail.com>
2021-12-02 16:14:20 -0800

    Got em all!

--- index.js
+++ index.js
@@ -41,14 +41,20 @@ export const printUserInfo = ({
 // REQS: use rest parameters
 //  getSum(1, 2, 3) === 6
 //  getSum(1, 2, 3, 4, 5) === 15
-export const getSum = () => {};
+export const getSum = (...rest) => {
+  let sum = 0;
+  rest.forEach((num) => (sum += num));
+  return sum;
+};
 
 // INPUT: an unknown number of arguments
 // OUTPUT: an array with the first two arguments destructured and the remaining in a nested array
 // REQS: use rest parameters
 // getFirstTwoArgs(1, 2, 3, 4, 5) should return [1, 2, [3, 4, 5]]
 // getFirstTwoArgs('a', 'b', 'c', 'd') should return ['a', 'b', ['c', 'd']]
-export const getFirstTwoArgs = () => {};
+export const getFirstTwoArgs = (a, b, ...rest) => {
+  return [a, b, rest];
+};
 
 // INPUT: an object with the following structure
 // {
@@ -71,16 +77,22 @@ export const getFirstTwoArgs = () => {};
 //    return a NEW object, do not modify the object passed in to the function
 //    use spread operator to create a new object
 
-export const addSneakerCount = () => {};
+export const addSneakerCount = ({ shoes, ...rest }) => {
+  return { ...rest, shoes, sneakerCount: shoes.length };
+};
 
 // INPUT: brands from data.js
 // OUTPUT: the brand names listed
 // REQS: use Object.keys to solve
-export const getBrandNames = () => {};
+export const getBrandNames = (brands) => {
+  return Object.keys(brands);
+};
 
 // INPUT: brands from data.js
 // OUTPUT: total number of sneaker types across all brands (14)
-export const totalSneakerCount = () => {};
+export const totalSneakerCount = (brands) => {
+  return Object.keys(brands).reduce((acc, key) => (acc += brands[key].shoes.length), 0);
+};
 
 // INPUT: An object
 // OUTPUT: An array with key value pairs converted to arrays
@@ -89,4 +101,6 @@ export const totalSneakerCount = () => {};
 // convertToArray({}) => []
 // Source: https://edabit.com/challenge/pPNAs5PvB3WvnDwDM
 
-export const convertToArray = () => {};
+export const convertToArray = (obj) => {
+  return Object.entries(obj);
+};
